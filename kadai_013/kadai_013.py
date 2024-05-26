c59dd78a547ca372f8c734accd57cc125ec19132
soh-po <soheiozawa0174@gmail.com>
2024-05-26 14:40:07 +0900

    Update kadai_013.py

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,7 +1,6 @@
 def calculate_total(price: int, tax_rate: float) -> int:
     total_price = price * (1 + tax_rate)
-
-    print(f"{total_price}円")
+    return total_price
     
 
-calculate_total(3000, 0.1)
+print(f"{calculate_total(3000, 0.1)}円")
